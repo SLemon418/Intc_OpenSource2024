9a76c41e7c58723ca3ad00a67c34002e88ca0c25
SLemon418 <ardor418@gmail.com>
2024-09-27 18:07:44 +0900

    add week05 main.go

---
+++ week05/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 55
+	var f float32 = 1.92
+
+	fmt.Println("f is", f)
+	fmt.Println("i is", i)
+	fmt.Print("i is ", i, "\n")
+	fmt.Println("i is", i)
+	fmt.Printf("i is %d\n", i)
+}
