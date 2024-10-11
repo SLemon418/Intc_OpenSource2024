df1605479cb8bca74f3ea65726cf28882990af85
SLemon418 <ardor418@gmail.com>
2024-10-11 17:06:57 +0900

    v0.8 add week07 main.go

---
+++ week07/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	var now = time.Now()
+	year := now.Year()
+	month := now.Month()
+	day := now.Day()
+	fmt.Println(int(month))
+	fmt.Println("오늘은 ", year, "년", int(month), "월", day, "일 입니다.")
+	fmt.Println("지금 시간은 ", now.Hour(), "시", now.Minute(), "분", now.Second(), "초 입니다.")
+
+	var army = "우리는 !가와 !민에 충성을 다하는 대한민! 육군이다"
+	armyFixed := strings.NewReplacer("!", "국")
+	fmt.Println(army)
+	fmt.Println(armyFixed.Replace(army))
+
+}
