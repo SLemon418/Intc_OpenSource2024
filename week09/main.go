b042e491a39d353c34a931d27685c26c8efd1ed0
SLemon418 <ardor418@gmail.com>
2024-10-25 17:01:16 +0900

    v1.7 guess game

--- week09/main.go
+++ week09/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -10,4 +14,24 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	answer := rand.Intn(6) + 1
 	fmt.Println("주사위 값", answer)
+
+	for guesses := 0; guesses < 3; guesses++ {
+		fmt.Print("주사위 값을 추측해보세요: ")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("숫자만 입력하세요.")
+		} else {
+			if guess == answer {
+				fmt.Println("맞았습니다!")
+				break
+			} else if guess < answer {
+				fmt.Println("틀렸습니다. 추측한 값이 작습니다.")
+			} else {
+				fmt.Println("틀렸습니다. 추측한 값이 큽니다.")
+			}
+		}
+	}
 }
